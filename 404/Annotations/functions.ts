c1adc51a31df20ed1284e7e54d7c20c337ad3e9e
erika-goodwin <shellvoerika@gmail.com>
2021-12-02 13:11:26 -0800

    502 file made

--- 404/Annotations/functions.ts
+++ 404/Annotations/functions.ts
@@ -1,38 +1,40 @@
-const add = (a:number,b:number) : number=> {
+const add = (a:number, b:number): number => {
     // return a + b + '10'
     return a + b + 10
 }
+
 console.log(add(2,3));
 
-
-// ===============================================
-const logger = (message:string) :void=> {
-    console.log(message);
-    return massage + '!!!!!'
+// ====================================================
+const logger = (message:string):void => {
+    console.log(message)
+    return message + "!!!!"
 }
-logger('HELLO THERE!!')
 
+logger("HELLO THERE!!")
 
-// ===============================================
-// * ?: option
-const throwError = (message?:string):void => {
+// ====================================================
+
+const throwError = (message?: string):void => {
     if(message){
         throw new Error(message)
     }
     throw new Error("DEFAULT ERROR MESSAGE")
-}
+} 
+
 throwError()
 
+// ====================================================
 
-// ===============================================
 const todaysWeather = {
     date: new Date(),
     weather: 'cloudy'
 }
-// { date:Date; weather: string }: inline interface
-const logWeather = ({date, weather}: {date:Date; weather: string }) => {
+
+const logWeather = ({ date, weather }: { date: Date; weather: string }) => {
     console.log(date);
     console.log(weather);
 }
 
 logWeather(todaysWeather)
+
---
+++ 502-clothing-ya-starter-main/README.md
@@ -0,0 +1,5 @@
+# After you fork or clone:
+
+## Install dependencies
+
+In your terminal after you clone your project down, remember to run either `yarn` or `npm install` to build all the dependencies in the project.
